742e928695f88ccb534c6a328f9b43174bcdfac8
oplichta <osi22@o2.pl>
2014-05-30 21:21:50 +0200

    Added more descripton of units

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -27,9 +27,24 @@ starcraftControllers.controller('UnitListCtrlTerran', ['$scope', 'UnitsTerran',
   starcraftControllers.controller('AddUnitCtrl', ['$scope', 'Units',
       function($scope, Units) {
         $scope.unitsT= Units.query();
-        
+
     }]);
 
+
+starcraftControllers.controller('ContactController', function ($scope, ContactService) {
+    $scope.contacts = ContactService.list();
+    $scope.saveContact = function () {
+        ContactService.save($scope.newcontact);
+        $scope.newcontact = {};
+    }
+    $scope.delete = function (id) {
+        ContactService.delete(id);
+        if ($scope.newcontact.id == id) $scope.newcontact = {};
+    }
+    $scope.edit = function (id) {
+        $scope.newcontact = angular.copy(ContactService.get(id));
+    }
+});
 
 starcraftControllers.controller('UnitDetailCtrl', ['$scope', '$routeParams', 'Units',
   function($scope, $routeParams, Units) {
--- app/js/services.js
+++ app/js/services.js
@@ -31,3 +31,43 @@ starcraftServices.factory('UnitsZerg', ['$resource',
         query: {method:'GET', params:{unitId:'units-terran'}, isArray:true}
       });
     }]);
+
+starcraftServices.service('ContactService', function () {
+    var i;
+    var uid = 1;
+    var contacts = [{
+        id: 0,
+        'name': 'Viral',
+        'email': 'hello@gmail.com',
+        'phone': '123-2343-44'
+    }];
+    this.save = function (contact) {
+        if (contact.id == null) {
+            contact.id = uid++;
+            contacts.push(contact);
+        } else {
+            for (i in contacts) {
+                if (contacts[i].id == contact.id) {
+                    contacts[i] = contact;
+                }
+            }
+        }
+    }
+    this.get = function (id) {
+        for (i in contacts) {
+            if (contacts[i].id == id) {
+                return contacts[i];
+            }
+        }
+    }
+    this.delete = function (id) {
+        for (i in contacts) {
+            if (contacts[i].id == id) {
+                contacts.splice(i, 1);
+            }
+        }
+    }
+    this.list = function () {
+        return contacts;
+    }
+});
