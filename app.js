072aed454110cda5345e3c9b5445955c57fba198
Oskar Plichta <osi22@o2.pl>
2016-04-23 14:04:40 +0200

    Updated app to use newest version of Angular and NodeJS

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ var appServer = require('./lib/server.js');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'app')));
-app.use(express.logger('dev'));
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
